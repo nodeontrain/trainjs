556920b99ae81fdf00f149b1372a14fb946bb457
Dang Thanh <thanhdd.it@gmail.com>
2013-07-29 22:10:46 +0700

    Check file exits; fix #1

--- bin/train_new.js
+++ bin/train_new.js
@@ -27,7 +27,11 @@ var diff = require('diff');
 var jroad = require('jroad');
 var readline = require('readline');
 
-var root_app, params, path_templ, lines;
+var root_app, params, path_templ, lines, file_type;
+var src_content, outStr;
+var count = 0;
+var order = 0;
+var question = false;
 var overwrite_all = false;
 
 function jtrain_toTitleCase (str) {
@@ -36,16 +40,25 @@ function jtrain_toTitleCase (str) {
     });
 }
 
-function create_file (src_content, des, file_name, message) {
-	var write_result = fs.writeFileSync(des, src_content);
-	if (typeof write_result == 'undefined')
+function create_file (src, des, file_name, message) {
+	if (file_type == 'image') {
+		var inStr = fs.createReadStream(src);
+		var outStr = fs.createWriteStream(des);
+		inStr.pipe(outStr);
 		console.log(message + file_name);
-	else
-		console.log(write_result);
+//		console.log(outStr._writableState);
+	} else {
+		var write_result = fs.writeFileSync(des, src_content);
+		if (typeof write_result == 'undefined')
+			console.log(message + file_name);
+		else
+			console.log(write_result);
+	}
 }
 
-function create_app (order) {
+function create_app () {
 	for (var i = order; i < lines.length; i++) {
+		count++;
 		var line = lines[i].split(" " + path_templ + "/");
 		var des = root_app + "/" + line[1];
 		var src = path_templ + "/" + line[1];
@@ -58,11 +71,11 @@ function create_app (order) {
 		} else {
 			if (line[1] == 'app/assets/images/trainjs.png' ||
 				line[1] == 'public/favicon.ico') {
-				var inStr = fs.createReadStream(src);
-				var outStr = fs.createWriteStream(des);
-				inStr.pipe(outStr);
-				console.log('      create  '.bold.green + line[1]);
+				file_type = "image";
+				var src_buff = fs.readFileSync(src);
+				src_content = JSON.stringify(src_buff);
 			} else {
+				file_type = "text";
 				var info_render = {};
 				if(line[1] == 'app/views/layouts/application.ejs')
 					info_render.title = jtrain_toTitleCase(params.app_name);
@@ -71,45 +84,55 @@ function create_app (order) {
 				else if(line[1] == 'public/index.html')
 					info_render = params;
 					
-				var src_content = fs.readFileSync(src).toString();
+				src_content = fs.readFileSync(src).toString();
 				for (var k in info_render) {
 					var reg = new RegExp("%%" + k + "%%", "g");		
 					src_content = src_content.replace(reg, info_render[k]);
 				}
-				
-				if (typeof src_content != 'undefined') {
-					if (fs.existsSync(des)) {
-						var des_content = fs.readFileSync(des).toString();
-						if (src_content == des_content) {
-							console.log('   identical  '.bold.blue + line[1]);
-						} else if (src_content != des_content && overwrite_all == true) {
-							console.log('    conflict  '.bold.red + line[1])
-							var message = '       force  '.bold.yellow;
-							create_file(src_content, des, line[1], message);
-						} else if (src_content != des_content && overwrite_all == false) {
-							order = i + 1;
-							console.log('    conflict  '.bold.red + line[1])
-							console.log('Overwrite '+ des +'? (enter "h" for help) [Ynaqdh]');
-							conflict(order, src_content, des_content, des, line[1]);
-							break;
-						}
-					} else {
-						var message = '      create  '.bold.green;
-						create_file (src_content, des, line[1], message);
-					}
+			}
+
+			if (fs.existsSync(des)) {
+				if (file_type == "text")
+					var des_content = fs.readFileSync(des).toString();
+				else {
+					var des_buff = fs.readFileSync(des);
+					var des_content = JSON.stringify(des_buff);
 				}
 
+				if (src_content == des_content) {
+					console.log('   identical  '.bold.blue + line[1]);
+				} else if (src_content != des_content && overwrite_all == true) {
+					console.log('    conflict  '.bold.red + line[1])
+					var message = '       force  '.bold.yellow;
+					create_file(src, des, line[1], message);
+				} else if (src_content != des_content && overwrite_all == false) {
+					order = i + 1;
+					question = true;
+					console.log('    conflict  '.bold.red + line[1])
+					console.log('Overwrite '+ des +'? (enter "h" for help) [Ynaqdh]');
+					break;
+				}
+			} else {
+				var message = '      create  '.bold.green;
+				create_file(src, des, line[1], message);
 			}
 		}
 	}
+	if (count == lines.length)
+		rl.close();
 }
 
-function conflict (order, src_content, des_content, des, file_name) {
-	var rl = readline.createInterface({
-		input: process.stdin,
-		output: process.stdout
-	});
-	rl.on('line', function (key) {
+var rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+	terminal: false
+});
+rl.on('line', function (key) {
+	if (question) {
+		var line = lines[order - 1].split(" " + path_templ + "/");
+		var file_name = line[1];
+		var des = root_app + "/" + line[1];
+		var src = path_templ + "/" + line[1];
 		if (key == "h") {
 			console.log('Y - yes, overwrite');
 			console.log('n - no, do not overwrite');
@@ -119,30 +142,34 @@ function conflict (order, src_content, des_content, des, file_name) {
 			console.log('h - help, show this help');
 		} else if (key == "y" || key == "Y") {
 			var message = '       force  '.bold.yellow;
-			create_file(src_content, des, file_name, message);
+			create_file(src, des, file_name, message);
 			create_app(order);
-			rl.close();
 		} else if (key == "n") {
 			var message = '        skip  '.bold.yellow + file_name;
 			console.log(message);
 			create_app (order);
-			rl.close();
 		} else if (key == "a") {
 			overwrite_all = true;
 			var message = '       force  '.bold.yellow;
-			create_file(src_content, des, file_name, message);
+			create_file(src, des, file_name, message);
 			create_app(order);
 			rl.close();
 		} else if (key == "q") {
 			console.log('Aborting...');
 			rl.close();
 		} else if (key == "d") {
-			var diff_result = diff.createPatch(des, des_content, src_content);
-			console.log(diff_result);
+			if (file_type == "text") {
+				var des_content = fs.readFileSync(des).toString();
+				var diff_result = diff.createPatch(des, des_content, src_content);
+				console.log(diff_result);
+			} else {
+				console.log(des + ' is binary file');
+			}
 			console.log('Overwrite '+ des +'? (enter "h" for help) [Ynaqdh]');
 		}
-	});
-}
+	}
+});
+
 
 module.exports = function(info_param) {
 	params = info_param;
@@ -158,5 +185,5 @@ module.exports = function(info_param) {
 	}
 
 	lines = jroad.list_files(path_templ);
-	create_app (0);
+	create_app();
 }
