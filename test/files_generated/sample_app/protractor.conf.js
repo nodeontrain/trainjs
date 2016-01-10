9f8be4b0871421556f4fe52fafce15b132ec1709
Dang Thanh <thanhdd.it@gmail.com>
2016-01-10 16:15:24 +0700

    test(v2.0.15)

--- test/files_generated/sample_app/protractor.conf.js
+++ test/files_generated/sample_app/protractor.conf.js
@@ -2,7 +2,7 @@ exports.config = {
 	framework: 'jasmine',
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: [
-		'test/e2e/*.js'
+		'public/test/e2e/*.js'
 	],
 	capabilities: {
 		'browserName': 'firefox'
